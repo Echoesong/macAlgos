a3be3a9ece152f79a89e556fd477af183d22b9ce
Joseph Felker <jayefelker@gmail.com>
2023-07-18 16:31:56 -0500

    valid_anagram python solution

---
+++ valid_anagram.py
@@ -0,0 +1,18 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+
+def valid_anagram(string, test):
+    string_list = list(string)
+    test_list = list(test)
+    string_list.sort()
+    test_list.sort()
+    if string_list == test_list:
+        return True
+    else:
+        return False
+
+print(valid_anagram('one', 'eno'))
